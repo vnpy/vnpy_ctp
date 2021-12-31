9f6b3d28aa7f39472b4ba003d0d657b4ff985950
renshiqiang <1140623243@qq.com>
2021-12-31 16:47:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,78 +17,44 @@ def get_ext_modules() -> list:
     ]
     extra_link_args = ["-lstdc++"]
     runtime_library_dirs = ["$ORIGIN"]
-    if platform.system() == "Linux":
-        vnctpmd = Extension(
-            "vnpy_ctp.api.vnctpmd",
-            [
-                "vnpy_ctp/api/vnctp/vnctpmd/vnctpmd.cpp",
-            ],
-            include_dirs=["vnpy_ctp/api/include",
-                        "vnpy_ctp/api/vnctp"],
-            define_macros=[],
-            undef_macros=[],
-            library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
-            libraries=["thostmduserapi_se", "thosttraderapi_se"],
-            extra_compile_args=compiler_flags,
-            extra_link_args=extra_link_args,
-            runtime_library_dirs=runtime_library_dirs,
-            depends=[],
-            language="cpp",
-        )
 
-        vnctptd = Extension(
-            "vnpy_ctp.api.vnctptd",
-            [
-                "vnpy_ctp/api/vnctp/vnctptd/vnctptd.cpp",
-            ],
-            include_dirs=["vnpy_ctp/api/include",
-                        "vnpy_ctp/api/vnctp"],
-            define_macros=[],
-            undef_macros=[],
-            library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
-            libraries=["thostmduserapi_se", "thosttraderapi_se"],
-            extra_compile_args=compiler_flags,
-            extra_link_args=extra_link_args,
-            runtime_library_dirs=runtime_library_dirs,
-            depends=[],
-            language="cpp",
-        )
+    vnctpmd = Extension(
+        "vnpy_ctp.api.vnctpmd",
+        [
+            "vnpy_ctp/api/vnctp/vnctpmd/vnctpmd.cpp",
+        ],
+        include_dirs=["vnpy_ctp/api/include",
+                    "vnpy_ctp/api/vnctp"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=compiler_flags if platform.system() == "Linux" else [],
+        extra_link_args=extra_link_args if platform.system() == "Linux" else [],
+        runtime_library_dirs=runtime_library_dirs if platform.system() == "Linux" else [],
+        depends=[],
+        language="cpp",
+    )
 
-        return [vnctptd, vnctpmd]
-    elif platform.system() == "Windows":
-        vnctpmd = Extension(
-            "vnpy_ctp.api.vnctpmd",
-            [
-                "vnpy_ctp/api/vnctp/vnctpmd/vnctpmd.cpp",
-            ],
-            include_dirs=["vnpy_ctp/api/include",
-                        "vnpy_ctp/api/vnctp"],
-            define_macros=[],
-            undef_macros=[],
-            library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
-            libraries=["thostmduserapi_se", "thosttraderapi_se"],
-            depends=[],
-            language="cpp",
-        )
+    vnctptd = Extension(
+        "vnpy_ctp.api.vnctptd",
+        [
+            "vnpy_ctp/api/vnctp/vnctptd/vnctptd.cpp",
+        ],
+        include_dirs=["vnpy_ctp/api/include",
+                    "vnpy_ctp/api/vnctp"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=compiler_flags if platform.system() == "Linux" else [],
+        extra_link_args=extra_link_args if platform.system() == "Linux" else [],
+        runtime_library_dirs=runtime_library_dirs if platform.system() == "Linux" else [],
+        depends=[],
+        language="cpp",
+    )
 
-        vnctptd = Extension(
-            "vnpy_ctp.api.vnctptd",
-            [
-                "vnpy_ctp/api/vnctp/vnctptd/vnctptd.cpp",
-            ],
-            include_dirs=["vnpy_ctp/api/include",
-                        "vnpy_ctp/api/vnctp"],
-            define_macros=[],
-            undef_macros=[],
-            library_dirs=["vnpy_ctp/api/libs", "vnpy_ctp/api"],
-            libraries=["thostmduserapi_se", "thosttraderapi_se"],
-            depends=[],
-            language="cpp",
-        )
-        
-        return [vnctptd, vnctpmd]
-    else:
-        return []
+    return [vnctptd, vnctpmd]
 
     
 setup(
